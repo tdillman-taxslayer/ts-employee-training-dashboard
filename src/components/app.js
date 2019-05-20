993cdf5c6046f29c8af67f3ba91b8bdb66ff717c
Timothy Dillman <timothy.dillman@mobilefirst.me>
2019-05-20 07:45:36 -0400

    added simple routing

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,11 @@
 import { h, Component } from "preact";
-import { Router } from "preact-router";
+import { Router, Route, route } from "preact-router";
 
 // Code-splitting is automated for routes
-import SideBar from "./sidebar";
+import SideBar, { RouteIDs } from "./sidebar";
 import { Grid } from "semantic-ui-react";
 import MyDashboard from "../routes/MyDashboard";
+import Library from "../routes/Library";
 
 export default class App extends Component {
   /** Gets fired when the route changes.
@@ -15,13 +16,25 @@ export default class App extends Component {
     this.currentUrl = e.url;
   };
 
+  handleSideBarOnClick(id) {
+    switch (id) {
+      case RouteIDs.DASHBOARD:
+        route("/", true);
+        break;
+      case RouteIDs.LIBRARY:
+        route("/library", true);
+        break;
+    }
+  }
+
   render() {
     return (
       <div id="app">
-        <SideBar />
+        <SideBar onClickHandler={this.handleSideBarOnClick} />
         <div className={"AppContainer"}>
           <Router onChange={this.handleRoute}>
-            <MyDashboard path="/" />
+            <Route path="/" component={MyDashboard} />
+            <Route path="/library" component={Library} />
           </Router>
         </div>
       </div>
